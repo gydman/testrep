2d24f669611dc795193be7cad4f13541da28babe
gydman <77411526+gydman@users.noreply.github.com>
2021-01-14 03:27:39 +0300

    Update vk_test_bot.py

--- vk_test_bot.py
+++ vk_test_bot.py
@@ -10,6 +10,99 @@ from auth_data import token
 from func_and_var import *
 
 # Создание переменной с функцией вызова клавиатуры (одно нажатие)
+keyboard = VkKeyboard(one_time=True)
+# Создание кнопок
+keyboard.add_button('a', color=VkKeyboardColor.PRIMARY)
+# перенос кнопки на следующую строку
+keyboard.add_line()
+keyboard.add_button('s', color=VkKeyboardColor.POSITIVE)
+keyboard.add_button('p', color=VkKeyboardColor.NEGATIVE)
+keyboard.add_line()
+keyboard.add_openlink_button('Ссылка', link='https://github.com/gydman')
+
+
+# отправить ответ на новое сообщение
+def write_message(sender, message):
+    vk_session.method('messages.send', {
+        'user_id': sender,
+        'message': message+' '+str(list_smile[random.randint(0, len(list_smile))]),
+        'random_id': get_random_id()
+        }
+    )
+
+
+# отправить картинку
+def send_photo(sender):
+    vk_session.method('messages.send', {
+        'user_id': sender,
+        'attachment': ','.join(attachments),
+        'random_id': get_random_id()
+        }
+    )
+    vk_session.method('messages.send', {
+        'user_id': sender,
+        'sticker_id': 128,
+        'random_id': get_random_id()
+        }
+    )
+    write_message(sender, message='Отличная картинка')
+
+
+def view_keybord(sender, message, keyboard):
+    vk_session.method('messages.send', {
+        'user_id': sender,
+        'message': message,
+        'random_id': 0,
+        'keyboard': keyboard.get_keyboard()
+        }
+    )
+
+
+vk_session = vk_api.VkApi(token=token)
+longpoll = VkLongPoll(vk_session)
+
+image = "C:/test/1.jpg"
+upload = VkUpload(vk_session)
+
+while True:
+    # Прослушиваем сервер
+    for event in longpoll.listen():
+        # Обработка нового сообщения
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            # для сообщения
+            received_message = event.text.lower()
+            sender = event.user_id
+
+            # для картинки
+            attachments = []
+            upload_image = upload.photo_messages(photos=image)[0]
+            attachments.append('photo{}_{}'.format(upload_image['owner_id'], upload_image['id']))
+
+            # анализ полученного текста и реакция
+            if received_message == 'a':
+                write_message(sender, list_answer['A'][random.randint(0, len(list_answer['A'][:]) - 1)])
+            elif received_message == 's':
+                write_message(sender, list_answer['B'][random.randint(0, 4)])
+            elif received_message == 'p':
+                send_photo(sender)
+            elif received_message == 'k':
+                view_keybord(sender, message='Клавиатура', keyboard=keyboard)
+            else:
+                write_message(sender, list_answer['C'][random.randint(0, 4)])
+
+        time.sleep(1)
+ vk_api
+import time
+import random
+from vk_api import VkUpload
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
+from auth_data import token
+from func_and_var import *
+
+# Создание переменной с функцией вызова клавиатуры (одно нажатие)
 keyboard = VkKeyboard(one_time=False)
 # Создание кнопок
 keyboard.add_button('a', color=VkKeyboardColor.PRIMARY)
